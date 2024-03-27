6596df5685831ab246062b5756842481b8dff216
TheWatcher01 <7450@holbertonstudents.com>
2024-03-27 19:00:05 +0100

    Update storage engine initialization

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,17 @@
-#!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+#!/usr/bin/env python3
+"""
+File: __init__.py
+Author: TheWatcher01
+Date: 2024-03-27
+Description: Initialize the storage engine
+"""
+from os import getenv
 
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 
-storage = FileStorage()
 storage.reload()
