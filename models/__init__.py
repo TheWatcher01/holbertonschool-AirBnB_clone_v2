d1fb61208757c6e250c37f21ca302a8e00c79d11
TheWatcher01 <7450@holbertonstudents.com>
2024-03-27 22:16:13 +0100

    Update storage engine initialization

--- models/__init__.py
+++ models/__init__.py
@@ -1,17 +1,23 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
-File: __init__.py
-Author: TheWatcher01
+Module: __init__.py
+Author: Teddy Deberdt
 Date: 2024-03-27
-Description: Initialize the storage engine
+Description: This module initializes the storage engine based on the
+environment variable 'HBNB_TYPE_STORAGE'.
 """
 from os import getenv
 
+# Check the environment variable 'HBNB_TYPE_STORAGE' to determine the type
+# of storage to use.
 if getenv('HBNB_TYPE_STORAGE') == 'db':
+    # If 'HBNB_TYPE_STORAGE' is 'db', use the DBStorage engine.
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
+    # If 'HBNB_TYPE_STORAGE' is not 'db', use the FileStorage engine.
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
 
+# Call the reload method on the storage engine to load any existing data.
 storage.reload()
