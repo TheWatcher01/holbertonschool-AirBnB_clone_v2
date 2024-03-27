85b21756bcf579706df1af06f7f8dc65af4742e8
TheWatcher01 <7450@holbertonstudents.com>
2024-03-27 14:49:30 +0100

    again

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,50 +1,80 @@
 #!/usr/bin/python3
 
 """
-File: test_console.py
-Author: Teddy Deberdt
+Fichier: test_console.py
+Auteur: Teddy Deberdt
 Date: 2024-03-25
-Description:
-
+Description: Tests pour vérifier les améliorations apportées à la console HBNB.
 """
-import unittest
-from unittest.mock import patch
-from io import StringIO
+
 from console import HBNBCommand
+from unittest.mock import patch, create_autospec
+from io import StringIO
+import unittest
+from models.engine.file_storage import FileStorage
+from models import storage
 
 
 class TestDoCreate(unittest.TestCase):
 
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_create_missing_class_name(self, mock_stdout):
-        """Test `do_create` when no class name is given, expecting a specific error message."""
+    def setUp(self):
+        """Configuration avant chaque test."""
+        self.patcher_out = patch('sys.stdout', new_callable=StringIO)
+        self.mock_stdout = self.patcher_out.start()
+        self.addCleanup(self.patcher_out.stop)
+        # Préparer un mock pour 'storage' qui simule les fonctionnalités de 'new' et 'save'
+        self.patcher_storage_new = patch.object(storage, 'new', autospec=True)
+        self.patcher_storage_save = patch.object(
+            storage, 'save', autospec=True)
+        self.mock_storage_new = self.patcher_storage_new.start()
+        self.mock_storage_save = self.patcher_storage_save.start()
+        self.addCleanup(self.patcher_storage_new.stop)
+        self.addCleanup(self.patcher_storage_save.stop)
+
+    def test_create_missing_class_name(self):
+        """Teste la réaction à l'absence du nom de classe."""
         HBNBCommand().do_create('')
-        self.assertEqual("** class name missing **\n", mock_stdout.getvalue())
+        self.assertEqual("** class name missing **\n",
+                         self.mock_stdout.getvalue())
 
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_create_class_does_not_exist(self, mock_stdout):
-        """Test `do_create` with an invalid class name, expecting an error message for non-existent class."""
+    def test_create_class_does_not_exist(self):
+        """Teste la réaction à un nom de classe invalide."""
         HBNBCommand().do_create('NonExistentClass')
-        self.assertEqual("** class doesn't exist **\n", mock_stdout.getvalue())
+        self.assertEqual("** class doesn't exist **\n",
+                         self.mock_stdout.getvalue())
 
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_create_attribute_format_error(self, mock_stdout):
-        """Test `do_create` with a malformed attribute string, checking for an attribute format error message."""
+    def test_create_attribute_format_error(self):
+        """Teste la réaction à un format d'attribut malformé."""
         HBNBCommand().do_create('User email="user@example.com" Password')
-        self.assertIn(
-            "** attribute format error **: Password (expected key=value)", mock_stdout.getvalue())
-        # Using `assertIn` to allow for partial match, given that the entire error message may vary.
+        expected_error = "** attribute format error **: Password (expected key=value)"
+        self.assertTrue(expected_error in self.mock_stdout.getvalue())
 
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_create_with_valid_attributes(self, mock_stdout):
-        """
-        Test for `do_create` with valid class name and attributes.
-        """
-        HBNBCommand().do_create('Place city_id="0001" user_id="0001" name="My_little_house" number_rooms=4 number_bathrooms=2 max_guest=10 price_by_night=300 latitude=37.773972 longitude=-122.431297')
-        # Here you should assert that the new object was correctly created and stored.
-        # This might require mocking the storage system or checking the output.
-        self.assertIn('76b65327-9e94-4632-b688-aaa22ab8a124',
-                      mock_stdout.getvalue())
+    def test_create_with_valid_attributes(self):
+        """Teste la création avec des attributs valides."""
+        cmd = 'Place city_id="0001" user_id="0001" name="My_little_house" number_rooms=4 number_bathrooms=2 max_guest=10 price_by_night=300 latitude=37.773972 longitude=-122.431297'
+        HBNBCommand().do_create(cmd)
+        # Assurez-vous que la fonction 'new' de 'storage' est appelée au moins une fois
+        self.assertTrue(self.mock_storage_new.called)
+        # Assurez-vous que la fonction 'save' de 'storage' est également appelée pour sauvegarder l'objet
+        self.assertTrue(self.mock_storage_save.called)
+
+    def test_create_with_mixed_types_attributes(self):
+        """Teste la création avec des attributs de types mixtes."""
+        cmd = 'Place name="My_little_house" number_rooms=4 latitude=37.773972 longitude=-122.431297'
+        HBNBCommand().do_create(cmd)
+        # Insérez des vérifications spécifiques ici
+
+    def test_create_with_complex_string_attributes(self):
+        """Teste la création avec des chaînes complexes comme attributs."""
+        cmd = 'Place name="\"My little house\""'
+        HBNBCommand().do_create(cmd)
+        # Insérez des vérifications spécifiques ici
+
+    def test_create_with_incomplete_attributes(self):
+        """Teste la création avec des spécifications d'attributs incomplètes."""
+        HBNBCommand().do_create('User email=')
+        self.assertIn("** attribute format error **",
+                      self.mock_stdout.getvalue())
 
 
 if __name__ == "__main__":
