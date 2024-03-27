92d6c6a7fda136d2283f804ad04fcfdc59995631
TheWatcher01 <7450@holbertonstudents.com>
2024-03-27 18:44:10 +0100

    Add user.py

--- models/user.py
+++ models/user.py
@@ -1,11 +1,19 @@
-#!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
+#!/usr/bin/env python3
+"""
+File: user.py
+Author: TheWatcher01
+Date: 2024-03-27
+Description: This file contains the User class.
+"""
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
