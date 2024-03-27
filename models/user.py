25f3559fb7a404d0cfa311a51fbd72e77a08e904
TheWatcher01 <7450@holbertonstudents.com>
2024-03-27 18:49:47 +0100

    restore user.py

--- models/user.py
+++ models/user.py
@@ -1,19 +1,11 @@
-#!/usr/bin/env python3
-"""
-File: user.py
-Author: TheWatcher01
-Date: 2024-03-27
-Description: This file contains the User class.
-"""
-from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String
+#!/usr/bin/python3
+"""This module defines a class User"""
+from models.base_model import BaseModel
 
 
-class User(BaseModel, Base):
+class User(BaseModel):
     """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
