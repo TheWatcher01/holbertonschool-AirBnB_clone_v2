22393e7434a78e289cc917acf7169fd1942db7ab
TheWatcher01 <7450@holbertonstudents.com>
2024-03-28 05:50:28 +0100

    Add fallback to FileStorage if required environment variables are missing for DBStorage

--- models/__init__.py
+++ models/__init__.py
@@ -13,14 +13,32 @@ from os import getenv
 # Determine the storage type from the 'HBNB_TYPE_STORAGE' environment variable
 storage_type = getenv('HBNB_TYPE_STORAGE')
 
+# Define required environment variables for DBStorage
+required_vars = ['HBNB_MYSQL_USER', 'HBNB_MYSQL_PWD',
+                 'HBNB_MYSQL_HOST', 'HBNB_MYSQL_DB']
+
 if storage_type == 'db':
-    # Use the database storage engine if 'HBNB_TYPE_STORAGE' is 'db'
-    from models.engine.db_storage import DBStorage
-    storage = DBStorage()
+    # Check if all required configurations for DBStorage are present
+    missing_vars = [var for var in required_vars if not getenv(var)]
+    if missing_vars:
+        print(
+            f"Missing env. variables for DBStorage: {', '.join(missing_vars)}")
+        print("Falling back to FileStorage.")
+        from models.engine.file_storage import FileStorage
+        storage = FileStorage()
+    else:
+        from models.engine.db_storage import DBStorage
+        storage = DBStorage()
 else:
-    # Default to file storage if 'HBNB_TYPE_STORAGE' is not 'db'
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
 
-# Initialize the storage engine to load any pre-existing data
-storage.reload()
+try:
+    # Initialize the storage engine to load any pre-existing data
+    storage.reload()
+except Exception as e:
+    print(f"Error loading storage: {e}")
+    if storage_type == 'db':
+        print("Make sure database is accessible & credentials are correct.")
+    else:
+        print("Check the integrity of your JSON file.")
