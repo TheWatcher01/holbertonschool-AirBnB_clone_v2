3945d76aa29c1e53d99f62513f8afed6629af42c
TheWatcher01 <7450@holbertonstudents.com>
2024-03-28 06:27:46 +0100

    Refactor DBStorage class in db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,93 +1,49 @@
-#!/usr/bin/python3
-"""
-Module: db_storage.py
-Author: Teddy Deberdt
-Date: 2024-03-25
-Description: Defines the DBStorage class interacting with MySQL via SQLAlchemy.
-"""
 from sqlalchemy import create_engine
-from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import scoped_session, sessionmaker
 from models.base_model import Base
 from os import getenv
 
 
 class DBStorage:
-    """
-    Handles interactions with the MySQL database through SQLAlchemy.
-    """
     __engine = None
     __session = None
 
     def __init__(self):
-        """
-        Initializes DBStorage instance, linking it to the MySQL database.
-        """
+        """Instantiate a DBStorage object."""
         user = getenv('HBNB_MYSQL_USER')
         pwd = getenv('HBNB_MYSQL_PWD')
         host = getenv('HBNB_MYSQL_HOST')
         db = getenv('HBNB_MYSQL_DB')
-        self.__engine = create_engine(
-            f'mysql+mysqldb://{user}:{pwd}@{host}/{db}',
-            pool_pre_ping=True)
-
-        if getenv('HBNB_ENV') == 'test':
-            Base.metadata.drop_all(self.__engine)
+        self.__engine = create_engine(f'mysql+mysqldb://{user}:{pwd}@{host}/{db}',
+                                      pool_pre_ping=True)
 
     def all(self, cls=None):
-        """
-        Queries all objects of a class, or all types if none specified.
-        """
-        try:
-            objects = self.__session.query(cls).all() if cls else [
-                obj for cls in Base._decl_class_registry.values()
-                if hasattr(cls, '__tablename__')
-                for obj in self.__session.query(cls).all()
-            ]
-            return {f"{type(obj).__name__}.{obj.id}": obj
-                    for obj in objects}
-        except SQLAlchemyError as e:
-            print(f"SQLAlchemy Exception: {e}")
-            return {}
+        """Query all objects depending on the class name."""
+        # Exemple de mise en œuvre, ajustez selon vos modèles
+        all_objs = {}
+        if cls:
+            objs = self.__session.query(cls).all()
+            for obj in objs:
+                key = f'{obj.__class__.__name__}.{obj.id}'
+                all_objs[key] = obj
+        return all_objs
 
     def new(self, obj):
-        """
-        Adds an object to the current database session.
-        """
+        """Add the object to the current database session."""
         self.__session.add(obj)
 
     def save(self):
-        """
-        Commits all changes of the current database session.
-        """
-        try:
-            self.__session.commit()
-        except SQLAlchemyError as e:
-            self.__session.rollback()
-            print(f"SQLAlchemy Exception: {e}")
+        """Commit all changes of the current database session."""
+        self.__session.commit()
 
     def delete(self, obj=None):
-        """
-        Deletes an object from the current database session, if not None.
-        """
+        """Delete from the current database session."""
         if obj:
             self.__session.delete(obj)
 
     def reload(self):
-        """
-        Recreates all tables in the database and initializes the session.
-        """
-        try:
-            Base.metadata.create_all(self.__engine)
-            session_factory = sessionmaker(
-                bind=self.__engine, expire_on_commit=False)
-            Session = scoped_session(session_factory)
-            self.__session = Session()
-        except SQLAlchemyError as e:
-            print(f"SQLAlchemy Exception: {e}")
-
-    def close(self):
-        """
-        Closes the current session.
-        """
-        self.__session.close()
+        """Create all tables in the database and initialize the session."""
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(
+            bind=self.__engine, expire_on_commit=False)
+        self.__session = scoped_session(session_factory)()
