9691ca3c6a52a1e4779cf4ad1bcc3e842627c637
TheWatcher01 <7450@holbertonstudents.com>
2024-03-28 03:02:36 +0100

    backup init

--- models/__init__.py
+++ models/__init__.py
@@ -10,21 +10,17 @@ Description: Initializes the storage engine based on the environment variable
 
 from os import getenv
 
-# Map storage types to their corresponding classes
-storage_types = {
-    'db': 'models.engine.db_storage.DBStorage',
-    'file': 'models.engine.file_storage.FileStorage'
-}
-
 # Determine the storage type from the 'HBNB_TYPE_STORAGE' environment variable
-storage_type = getenv('HBNB_TYPE_STORAGE', 'file')
+storage_type = getenv('HBNB_TYPE_STORAGE')
 
-# Use the specified storage type, or default to file storage
-try:
-    storage_class = __import__(storage_types[storage_type], fromlist=[''])
-    storage = storage_class()
-except KeyError:
-    raise Exception("Invalid storage type: {}".format(storage_type))
+if storage_type == 'db':
+    # Use the database storage engine if 'HBNB_TYPE_STORAGE' is 'db'
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    # Default to file storage if 'HBNB_TYPE_STORAGE' is not 'db'
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 
 # Initialize the storage engine to load any pre-existing data
 storage.reload()
