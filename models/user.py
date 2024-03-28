8ca8e74bd4809b2384868b4ef5f1b5aecddce246
TheWatcher01 <7450@holbertonstudents.com>
2024-03-28 03:35:26 +0100

    cackup test

--- models/user.py
+++ models/user.py
@@ -1,27 +1,14 @@
 #!/usr/bin/python3
-"""
-Module: user.py
-Author: Teddy Deberdt
-Date: 2024-03-27
-Description: This module defines the User class, which inherits from BaseModel
-and represents a user in the HBNB project.
-"""
-from models.base_model import BaseModel, Base
+"""Defines the User class."""
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
 
 class User(BaseModel, Base):
-    """
-    The User class represents a user. It inherits from BaseModel and Base
-    (SQLAlchemy declarative base class for Table mapping).
-    Attributes:
-        email (str): The email of the user. It cannot be null.
-        password (str): The password of the user. It cannot be null.
-        first_name (str): The first name of the user. It can be null.
-        last_name (str): The last name of the user. It can be null.
-    """
+    """Represents a user for the HBNB project."""
     __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
