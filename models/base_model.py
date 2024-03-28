b67f062faa4a3d9d7013c8b06923fad4796298a7
TheWatcher01 <7450@holbertonstudents.com>
2024-03-28 04:13:01 +0100

    Refactor BaseModel class to use DateTime instead of DATETIME

--- models/base_model.py
+++ models/base_model.py
@@ -7,7 +7,7 @@ Description: This module defines the BaseModel class, which serves as the base
 class for all other model classes in the hbnb project.
 """
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import DATETIME, Column, String
+from sqlalchemy import Column, DateTime, String
 from datetime import datetime
 import uuid
 
@@ -21,43 +21,37 @@ class BaseModel:
     BaseModel converts Python classes into SQLAlchemy Table objects with
     appropriate fields and relationships for database storage.
     """
-    __abstract__ = True  # SQLAlchemy class isn't a database table itself
-    id = Column(String(60), nullable=False, primary_key=True, unique=True)
-    created_at = Column(DATETIME, default=datetime.utcnow(), nullable=False)
-    updated_at = Column(DATETIME, default=datetime.utcnow(), nullable=False)
+    __abstract__ = True  # Indicate that BaseModel isn't a database table itself
+    id = Column(String(60), primary_key=True, nullable=False, unique=True)
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow,
+                        onupdate=datetime.utcnow, nullable=False)
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, **kwargs):
         """
         Instantiate a new model.
-        If no kwargs are provided, id, created_at and updated_at are set.
         If kwargs are provided, they are used to set instance attributes.
+        Otherwise, id, created_at, and updated_at are set to their default values.
         """
         if not kwargs:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = self.updated_at = datetime.utcnow()
         else:
+            kwargs.setdefault('id', str(uuid.uuid4()))
+            kwargs.setdefault('created_at', datetime.utcnow())
+            kwargs.setdefault('updated_at', datetime.utcnow())
             for key, value in kwargs.items():
-                if key != '__class__':
-                    if key in ('created_at', 'updated_at'):
-                        setattr(self, key, datetime.fromisoformat(value))
-                    else:
-                        setattr(self, key, value)
-            # if os.getenv('HBNB_TYPE_STORAGE') in ('db'):
-            if not hasattr(kwargs, 'id'):
-                setattr(self, 'id', str(uuid.uuid4()))
-            if not hasattr(kwargs, 'created_at'):
-                setattr(self, 'created_at', datetime.now())
-            if not hasattr(kwargs, 'updated_at'):
-                setattr(self, 'updated_at', datetime.now())
+                if key in ('created_at', 'updated_at') and isinstance(value, str):
+                    value = datetime.fromisoformat(value)
+                setattr(self, key, value)
 
     def __str__(self):
         """
         Returns a string representation of the instance.
         Format: [<class name>] (<id>) <dictionary of instance attributes>
         """
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        cls_name = self.__class__.__name__
+        return '[{}] ({}) {}'.format(cls_name, self.id, self.__dict__)
 
     def save(self):
         """
@@ -79,10 +73,8 @@ class BaseModel:
         """
         Converts the instance into a dictionary format for JSON serialization.
         """
-        dictionary = self.__dict__.copy()
-        dictionary['__class__'] = self.__class__.__name__
-        if '_sa_instance_state' in dictionary:
-            del dictionary['_sa_instance_state']
-        dictionary['created_at'] = dictionary['created_at'].isoformat()
-        dictionary['updated_at'] = dictionary['updated_at'].isoformat()
+        dictionary = {**self.__dict__, '__class__': self.__class__.__name__}
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
+        dictionary.pop('_sa_instance_state', None)
         return dictionary
