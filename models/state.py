db4fefbedd6fbc43247ba32a19bf8427f3101eee
TheWatcher01 <7450@holbertonstudents.com>
2024-03-28 06:56:12 +0100

    Refactor state.py to city.py and update class name and table name

--- models/state.py
+++ models/state.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
 """
-Module: state.py
+Module: city.py
 Author: Teddy Deberdt
 Date: 2024-03-27
-Description: This module defines the State class, which inherits from BaseModel
-and represents a state in the HBNB project.
+Description: This module defines the City class, which inherits from BaseModel
+and represents a city in the HBNB project.
 """
+from sqlalchemy import Column, String, ForeignKey
 from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String
 
 
-class State(BaseModel, Base):
+class City(BaseModel, Base):
     """
-    The State class represents a state. It inherits from BaseModel and Base
+    The City class represents a city. It inherits from BaseModel and Base
     (SQLAlchemy declarative base class for Table mapping).
     Attributes:
-        name (str): The name of the state.
+        state_id (str): The state id to which the city belongs.
+        name (str): The name of the city.
     """
-    __tablename__ = 'states'
+    __tablename__ = 'cities'
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state",
-                          cascade="all, delete-orphan")
