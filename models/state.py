73c70b3c91315f199206779a8d16f372b56fdf67
TheWatcher01 <7450@holbertonstudents.com>
2024-03-28 10:39:38 +0100

    add state

--- models/state.py
+++ models/state.py
@@ -8,7 +8,8 @@ and represents a state in the HBNB project.
 """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-
+from sqlalchemy.orm import relationship
+from os import getenv
 
 class State(BaseModel, Base):
     """
@@ -19,3 +20,15 @@ class State(BaseModel, Base):
     """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", backref="state",
+                              cascade="all, delete, delete-orphan")
+    else:
+        @property
+        def cities(self):
+            """Retourne la liste des City instances liées au State pour FileStorage."""
+            from models import storage
+            all_cities = storage.all(City)
+            state_cities = [city for city in all_cities.values()
+                            if city.state_id == self.id]
+            return state_cities
