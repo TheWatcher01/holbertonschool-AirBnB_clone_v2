980d0876531e8a1b1c8afe07966470cd3f7e7498
TheWatcher01 <7450@holbertonstudents.com>
2024-03-28 09:29:56 +0100

    Test backuup city & state 051a912

--- models/state.py
+++ models/state.py
@@ -3,33 +3,22 @@
 Module: state.py
 Author: Teddy Deberdt
 Date: 2024-03-27
-Description: Module defines State class, which inherits from BaseModel
+Description: This module defines the State class, which inherits from BaseModel
 and represents a state in the HBNB project.
 """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from os import getenv
+from sqlalchemy import Column, String
 
 
 class State(BaseModel, Base):
     """
-    State class represents a state. It inherits from BaseModel and Base
+    The State class represents a state. It inherits from BaseModel and Base
     (SQLAlchemy declarative base class for Table mapping).
     Attributes:
         name (str): The name of the state.
     """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship("City", back_populates="state",
-                              cascade="all, delete-orphan")
-    else:
-        @property
-        def cities(self):
-            """Get a list of all related City objects."""
-            from models import storage
-            from models.city import City
-            return [city for city in storage.all(City).values()
-                    if city.state_id == self.id]
+    cities = relationship("City", backref="state",
+                          cascade="all, delete-orphan")
