ec3a6a801ef3542c2952622fd575315026f1f2ce
TheWatcher01 <7450@holbertonstudents.com>
2024-03-29 20:50:59 +0100

    Update MySQL setup scripts

--- setup_mysql_prod.sql
+++ setup_mysql_prod.sql
@@ -6,9 +6,7 @@ USE hbnb_prod_db;
 
 -- Create the user 'hbnb_prod' with the password 'hbnb_prod_pwd' if it does not already exist
 -- and set the authentication method to 'mysql_native_password'
-CREATE USER IF NOT EXISTS 'hbnb_prod' @'localhost' IDENTIFIED
-WITH
-    mysql_native_password BY 'hbnb_prod_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_prod' @'localhost' IDENTIFIED BY 'hbnb_prod_pwd';
 
 -- Grant all privileges on the 'hbnb_prod_db' database to the 'hbnb_prod' user
 GRANT ALL PRIVILEGES ON hbnb_prod_db.* TO 'hbnb_prod' @'localhost';
@@ -18,6 +16,3 @@ GRANT SELECT ON performance_schema.* TO 'hbnb_prod' @'localhost';
 
 -- Refresh any changes to the privileges
 FLUSH PRIVILEGES;
-
--- Test if the user and database are accessible
-SELECT DATABASE() AS "Current Database", USER () AS "Current User";
