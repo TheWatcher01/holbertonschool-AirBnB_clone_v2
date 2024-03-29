f59e17addab613ef962e1d246a01ecfdf6ff3f58
TheWatcher01 <7450@holbertonstudents.com>
2024-03-29 13:01:58 +0100

    Fix datetime import in base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -9,7 +9,7 @@ classes into SQLAlchemy Table objects for database storage.
 """
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DateTime
-from datetime import datetime
+import datetime
 import uuid
 
 Base = declarative_base()
