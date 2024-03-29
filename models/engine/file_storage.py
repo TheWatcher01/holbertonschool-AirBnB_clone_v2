2d60102bfd7b6de1b01f7151f2ef73bd76aa8a16
TheWatcher01 <7450@holbertonstudents.com>
2024-03-29 11:57:56 +0100

    restore new(self, obj) method to file_storage.py (broken the checker)

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,73 +3,48 @@
 Module: file_storage.py
 Author: Teddy Deberdt
 Date: 2024-03-27
-Description: This module defines the FileStorage class. It is responsible for
-serializing instances to a JSON file and deserializing JSON file to instances.
-It manages a simple file-based storage system.
+Description: Defines the FileStorage class for serializing instances to a JSON
+file and deserializing JSON file to instances, managing a simple file-based
+storage system.
 """
 
 import json
 
 
 class FileStorage:
-    """
-    This class manages the storage of hbnb models in JSON format.
-    """
+    """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
 
     def all(self, cls=None):
-        """
-        Returns a dictionary of models currently in storage.
-        If a class is provided, it returns only instances of that class.
-        """
+        """Returns a dictionary of models currently in storage"""
         if cls is None:
             return FileStorage.__objects
         else:
             return {k: v for k, v in FileStorage.__objects.items()
-                    if isinstance(v, cls)}
+                    if type(v) == cls}
 
     def delete(self, obj=None):
-        """
-        Deletes obj from __objects if it’s inside.
-        If obj is None, the method does nothing.
-        """
+        """Deletes obj from __objects if it’s inside"""
         if obj is not None:
             obj_key = "{}.{}".format(type(obj).__name__, obj.id)
             if obj_key in FileStorage.__objects:
                 del FileStorage.__objects[obj_key]
 
     def new(self, obj):
-        """
-        Adds a new object to the storage dictionary after validating it.
-        The object must have a 'required_fields' method/attribute.
-        """
-        if hasattr(obj, 'required_fields'):
-            required_fields = obj.required_fields()
-            obj_dict = obj.to_dict()
-            if not all(field in obj_dict for field in required_fields):
-                raise ValueError(
-                    f"Object of type {type(obj).__name__} is missing "
-                    "required fields.")
-            key = '{}.{}'.format(type(obj).__name__, obj.id)
-            self.all().update({key: obj})
-        else:
-            raise ValueError(
-                f"Object of type {type(obj).__name__} does not define "
-                "required fields.")
+        """Adds new object to storage dictionary"""
+        obj_dict = obj.to_dict()
+        key = '{}.{}'.format(obj_dict['__class__'], obj.id)
+        self.all().update({key: obj})
 
     def save(self):
-        """
-        Saves the storage dictionary to a file.
-        """
+        """Saves storage dictionary to file"""
         with open(FileStorage.__file_path, 'w') as f:
             temp = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
             json.dump(temp, f)
 
     def reload(self):
-        """
-        Loads the storage dictionary from a file.
-        """
+        """Loads storage dictionary from file"""
         from models.base_model import BaseModel
         from models.user import User
         from models.place import Place
