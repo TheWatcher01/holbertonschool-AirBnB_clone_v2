cd4879591e992ba63871fa574f1518ad5f10e4af
TheWatcher01 <7450@holbertonstudents.com>
2024-03-29 15:20:19 +0100

    Refactor City and State models

--- models/state.py
+++ models/state.py
@@ -6,10 +6,9 @@ Date: 2024-03-27
 Description: Defines the State class in the HBNB project, representing states
 with a name attribute and managing cities through a relationship or property.
 """
+from models.base_model import BaseModel
 
-from models.base_model import BaseModel, Base
 
-
-class State(BaseModel, Base):
+class State(BaseModel):
     """ State class """
     name = ""
