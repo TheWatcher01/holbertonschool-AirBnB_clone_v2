46e4d6af41297556258314875474fee0ce16e591
TheWatcher01 <7450@holbertonstudents.com>
2024-03-29 15:16:35 +0100

    Add Base class inheritance to City and State models

--- models/city.py
+++ models/city.py
@@ -6,10 +6,10 @@ Date: 2024-03-27
 Description: Defines the City class in the HBNB project. Represents cities
 with name and state_id attributes, extending functionality from BaseModel.
 """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     state_id = ""
     name = ""
--- models/state.py
+++ models/state.py
@@ -7,9 +7,9 @@ Description: Defines the State class in the HBNB project, representing states
 with a name attribute and managing cities through a relationship or property.
 """
 
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
     name = ""
