044c547c67f8f2ef4f485448a779d529b7c2e793
TheWatcher01 <7450@holbertonstudents.com>
2024-03-31 16:02:01 +0200

    Refactor test_review.py to allow None values for place_id, user_id, and text

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -16,14 +16,14 @@ class test_review(test_basemodel):
     def test_place_id(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.place_id), str)
+        self.assertTrue(type(new.place_id) == str or new.place_id is None)
 
     def test_user_id(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.user_id), str)
+        self.assertTrue(type(new.user_id) == str or new.user_id is None)
 
     def test_text(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.text), str)
+        self.assertTrue(type(new.text) == str or new.text is None)
