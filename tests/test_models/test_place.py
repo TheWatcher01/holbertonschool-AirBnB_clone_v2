074e27fe73a66523cd0ced8dd722ab5a79cf4495
TheWatcher01 <7450@holbertonstudents.com>
2024-03-31 23:23:44 +0200

    Refactor test_Place class in test_place.py

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -5,65 +5,65 @@ from models.place import Place
 
 
 class test_Place(test_basemodel):
-    """Tests for the Place model."""
+    """ """
 
     def __init__(self, *args, **kwargs):
-        """Initialize test case."""
+        """ """
         super().__init__(*args, **kwargs)
         self.name = "Place"
         self.value = Place
 
     def test_city_id(self):
-        """Test city_id attribute."""
+        """ """
         new = self.value()
-        self.assertIsNone(new.city_id)
+        self.assertEqual(type(new.city_id), str)
 
     def test_user_id(self):
-        """Test user_id attribute."""
+        """ """
         new = self.value()
-        self.assertIsNone(new.user_id)
+        self.assertEqual(type(new.user_id), str)
 
     def test_name(self):
-        """Test name attribute."""
+        """ """
         new = self.value()
-        self.assertIsNone(new.name)
+        self.assertEqual(type(new.name), str)
 
     def test_description(self):
-        """Test description attribute."""
+        """ """
         new = self.value()
-        self.assertIsNone(new.description)
+        self.assertEqual(type(new.description), str)
 
     def test_number_rooms(self):
-        """Test number_rooms attribute."""
+        """ """
         new = self.value()
-        self.assertEqual(new.number_rooms, 0)
+        self.assertEqual(type(new.number_rooms), int)
 
     def test_number_bathrooms(self):
-        """Test number_bathrooms attribute."""
+        """ """
         new = self.value()
-        self.assertEqual(new.number_bathrooms, 0)
+        self.assertEqual(type(new.number_bathrooms), int)
 
     def test_max_guest(self):
-        """Test max_guest attribute."""
+        """ """
         new = self.value()
-        self.assertEqual(new.max_guest, 0)
+        self.assertEqual(type(new.max_guest), int)
 
     def test_price_by_night(self):
-        """Test price_by_night attribute."""
+        """ """
         new = self.value()
-        self.assertEqual(new.price_by_night, 0)
+        self.assertEqual(type(new.price_by_night), int)
 
     def test_latitude(self):
-        """Test latitude attribute."""
+        """ """
         new = self.value()
-        self.assertIsNone(new.latitude)
+        self.assertEqual(type(new.latitude), float)
 
     def test_longitude(self):
-        """Test longitude attribute."""
+        """ """
         new = self.value()
-        self.assertIsNone(new.longitude)
+        self.assertEqual(type(new.latitude), float)
 
     def test_amenity_ids(self):
-        """Test amenity_ids attribute."""
+        """ """
         new = self.value()
-        self.assertEqual(new.amenity_ids, [])
+        self.assertEqual(type(new.amenity_ids), list)
