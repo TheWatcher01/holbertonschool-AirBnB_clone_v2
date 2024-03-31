403862f6c6a6fe830a14f2b576c68762ee982d3e
TheWatcher01 <7450@holbertonstudents.com>
2024-03-31 18:30:18 +0200

    Refactor test classes for Amenity and City models

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,34 @@
 #!/usr/bin/python3
-""" """
+"""
+Test module for the Amenity class.
+"""
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
 
 
-class test_Amenity(test_basemodel):
-    """ """
+class TestAmenity(test_basemodel):
+    """
+    Test class for verifying the Amenity model.
+    """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """
+        Initialize the tests for the Amenity class by specifying the model
+        name and class.
+        """
         super().__init__(*args, **kwargs)
-        self.name = "Amenity"
-        self.value = Amenity
+        self.model_name = "Amenity"
+        self.model_class = Amenity
 
-    def test_name2(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.name) == str or new.name is None, True)
+    def test_name_attribute(self):
+        """
+        Test that the 'name' attribute of an Amenity instance is always a
+        non-null string.
+        """
+        # Create an Amenity instance with a valid name.
+        amenity_with_name = self.model_class(name="Wifi")
+        # Verify that the 'name' attribute is a string and is not None.
+        self.assertIsInstance(amenity_with_name.name, str,
+                              "The 'name' attribute should be a string.")
+        self.assertIsNotNone(amenity_with_name.name,
+                             "The 'name' attribute should not be None.")
--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,24 +1,47 @@
 #!/usr/bin/python3
-""" """
+"""
+Test module for the City class.
+"""
 from tests.test_models.test_base_model import test_basemodel
 from models.city import City
 
 
-class test_City(test_basemodel):
-    """ """
+class TestCity(test_basemodel):
+    """
+    Test class for verifying the City model.
+    """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """
+        Initialize the tests for the City class by specifying the model
+        name and class.
+        """
         super().__init__(*args, **kwargs)
-        self.name = "City"
-        self.value = City
+        self.model_name = "City"
+        self.model_class = City
 
-    def test_state_id(self):
-        """ """
-        new = self.value()
-        self.assertTrue(type(new.state_id) == str or new.state_id is None)
+    def test_state_id_attribute(self):
+        """
+        Test that the 'state_id' attribute of a City instance is always a
+        non-null string.
+        """
+        # Create a City instance with a valid state_id.
+        city_with_state_id = self.model_class(state_id="some_state_id")
+        # Verify that the 'state_id' attribute is a string and is not None.
+        self.assertIsInstance(city_with_state_id.state_id, str,
+                              "The 'state_id' attribute should be a string.")
+        self.assertIsNotNone(city_with_state_id.state_id,
+                             "The 'state_id' attribute should not be None.")
 
-    def test_name(self):
-        """ """
-        new = self.value()
-        self.assertTrue(type(new.name) == str or new.name is None)
+    def test_name_attribute(self):
+        """
+        Test that the 'name' attribute of a City instance is always a
+        non-null string.
+        """
+        # Create a City instance with a valid name.
+        city_with_name = self.model_class(name="Test City")
+        # Verify that the 'name' attribute is a string and is not None.
+        self.assertIsInstance(city_with_name.name, str,
+                              "The 'name' attribute should be a string.")
+        self.assertIsNotNone(city_with_name.name,
+                             "The 'name' attribute should not be None.")
