104a570e1c68c5179c5966cbc2dc9ce582f28c6c
TheWatcher01 <7450@holbertonstudents.com>
2024-03-31 18:04:05 +0200

    Backup db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -11,14 +11,15 @@ operations, and session management tailored to the hbnb project's requirements.
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy import create_engine
-from models.base_model import Base
 from models.amenity import Amenity
-from models.city import City
+from models.base_model import Base
 from models.place import Place
 from models.review import Review
 from models.state import State
-from models.user import User
+from models.city import City
 from os import getenv
+
+from models.user import User
 
 
 class DBStorage:
@@ -31,7 +32,11 @@ class DBStorage:
     __session = None
 
     def __init__(self):
-        """Initializes the DBStorage instance."""
+        """
+        Initializes the DBStorage instance by creating an engine linked to
+        MySQL database specified via environment variables. It conditionally
+        drops all tables if the environment is set to 'test'.
+        """
         user = getenv('HBNB_MYSQL_USER')
         pwd = getenv('HBNB_MYSQL_PWD')
         host = getenv('HBNB_MYSQL_HOST')
@@ -40,46 +45,57 @@ class DBStorage:
         self.__engine = create_engine(conn_str, pool_pre_ping=True)
 
         if getenv('HBNB_ENV') == 'test':
-            # Drop all tables for a clean test environment
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """Queries and returns all objects of a given class or all objects."""
-        session = self.__session()
+        """
+        Queries and returns all objects of a given class from the database.
+        If no class is specified, it returns all objects in the database.
+        """
         if cls:
-            objects = session.query(cls).all()
+            return {f'{obj.__class__.__name__}.{obj.id}': obj
+                    for obj in self.__session.query(cls).all()}
         else:
-            classes = [State, City, User, Review, Amenity, Place]
-            objects = [
-                item for cls in classes for item in session.query(cls).all()]
-
-        return {f'{obj.__class__.__name__}.{obj.id}': obj for obj in objects}
+            result = {}
+            for cls in [State, City, User, Review, Amenity, Place]:
+                result.update(self.all(cls))
+            return result
 
     def new(self, obj):
-        """Adds a new object to the current database session."""
+        """
+        Adds a new object to the current database session, ready for commit.
+        """
         self.__session.add(obj)
 
     def save(self):
-        """Commits all changes to the database."""
+        """
+        Commits all changes of the current database session to the database.
+        Handles exceptions by rolling back the session to the previous state.
+        """
         try:
             self.__session.commit()
         except SQLAlchemyError as e:
             self.__session.rollback()
-            # Consider logging this exception as well
             print(f'SQLAlchemy Exception: {e}')
 
     def delete(self, obj=None):
-        """Deletes an object from the current database session."""
+        """
+        Deletes an object from the current database session, if it's not None.
+        """
         if obj:
             self.__session.delete(obj)
 
     def reload(self):
-        """Reloads all tables from the database and recreates the session."""
+        """
+        Recreates the database schema and initializes a new session.
+        """
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(
             bind=self.__engine, expire_on_commit=False)
-        self.__session = scoped_session(session_factory)()
+        self.__session = scoped_session(session_factory)
 
     def close(self):
-        """Closes the current SQLAlchemy session."""
+        """
+        Closes the current SQLAlchemy session, ensuring clean-up.
+        """
         self.__session.remove()
