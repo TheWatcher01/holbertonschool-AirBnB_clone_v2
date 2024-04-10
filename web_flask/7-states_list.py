fd5085406da9da0278b88d091f2a491826b56e3b
TheWatcher01 <7450@holbertonstudents.com>
2024-04-10 14:41:50 +0200

    Against test for task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,33 +9,23 @@ Description: This script initiates a Flask web application that listens on
 """
 
 from flask import Flask, render_template
-from models.state import State
 from models import storage
-
+from models.state import State
 
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
+@app.route('/states_list', strict_slashes=False)
 def states_list():
-    """
-    This route generates an HTML page that lists all states in the database.
-    """
-    states = storage.all(State)
+    states = list(storage.all(State).values())
+    states.sort(key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown_db(exception=None):
-    """
-    This function ensures that the database is closed when the request ends.
-    """
+def teardown_db(exception):
     storage.close()
 
 
 if __name__ == "__main__":
-    """
-    This conditional ensures that the Flask application only runs if the script
-    is executed directly and not used as an imported module.
-    """
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
